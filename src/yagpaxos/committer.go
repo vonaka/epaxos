3ebed05aa1f7e07f2ac58a9ab1154f1f2b3fafa2
vonaka <vonssung@gmail.com>
2018-12-18 17:08:33 +0100

    Code refactoring.

---
+++ src/yagpaxos/committer.go
@@ -0,0 +1,96 @@
+package yagpaxos
+
+import (
+	"errors"
+	"sync"
+	"time"
+	"yagpaxosproto"
+)
+
+type committer struct {
+	next      int
+	first     int
+	delivered int
+	cmdIds    map[int]int32
+	instances map[int32]int
+}
+
+func newCommitter(m *sync.Mutex) *committer {
+	c := &committer{
+		next:      0,
+		first:     -1,
+		delivered: -1,
+		cmdIds:    make(map[int]int32, 10), // FIXME
+		instances: make(map[int32]int, 10), // FIXME
+	}
+
+	go func() {
+		for {
+			time.Sleep(8 * time.Second) // FIXME
+			m.Lock()
+			for i := c.first + 1; i <= c.delivered; i++ {
+				// delete(c.instances, c.cmdIds[i])
+				// we can't delete c.instance so simply,
+				// as it can be used in getInstance
+				delete(c.cmdIds, i)
+			}
+			c.first = c.delivered
+			m.Unlock()
+		}
+	}()
+
+	return c
+}
+
+func (c *committer) getInstance(cmdId int32) int {
+	return c.instances[cmdId]
+}
+
+func (c *committer) add(cmdId int32) {
+	c.cmdIds[c.next] = cmdId
+	c.instances[cmdId] = c.next
+	c.next++
+}
+
+func (c *committer) addTo(cmdId int32, instance int) {
+	c.cmdIds[instance] = cmdId
+	c.instances[cmdId] = instance
+}
+
+func (c *committer) deliver(cmdId int32, f func(int32)) {
+	i := c.delivered + 1
+	j := c.instances[cmdId]
+	for ; i <= j; i++ {
+		f(c.cmdIds[i])
+	}
+
+	if j > c.delivered {
+		c.delivered = j
+	}
+}
+
+func (c *committer) safeDeliver(cmdId int32, cmdDeps yagpaxosproto.DepSet,
+	f func(int32)) error {
+	if cmdDeps.Iter(func(depId int32) bool {
+		_, exists := c.instances[depId]
+		return !exists
+	}) {
+		return errors.New("some dependency is no commited yet")
+	}
+	continuous := true
+	i := c.delivered + 1
+	j := c.instances[cmdId]
+	for ; i <= j; i++ {
+		_, exists := c.cmdIds[i]
+		if !exists {
+			continuous = false
+		} else {
+			f(c.cmdIds[i])
+			if continuous {
+				c.delivered = i
+			}
+		}
+	}
+
+	return nil
+}
---
+++ src/yagpaxos/quorum.go
@@ -0,0 +1,160 @@
+package yagpaxos
+
+import (
+	"sync"
+	"time"
+)
+
+type quorum struct {
+	// not the desired size, but the actual one
+	size     int
+	leaderId int
+	elements []interface{}
+}
+
+type quorumSet struct {
+	sync.Mutex
+	neededSize     int
+	totalSize      int
+	quorums        map[int]*quorum
+	leaderQuorum   *quorum
+	biggestQuorum  *quorum
+	related        func(interface{}, interface{}) bool
+	wakeupIf       func() bool
+	out            chan struct{}
+	stop           chan struct{}
+	called         bool
+	onlyWithLeader bool
+}
+
+func newQuorumSet(size int, waitFor time.Duration,
+	relation func(interface{}, interface{}) bool,
+	wakeupIf func() bool, handler func(*quorum),
+	onlyWithLeader bool) *quorumSet {
+	qs := &quorumSet{
+		neededSize:     size,
+		totalSize:      0,
+		quorums:        make(map[int]*quorum, size),
+		leaderQuorum:   nil,
+		biggestQuorum:  nil,
+		related:        relation,
+		wakeupIf:       wakeupIf,
+		out:            make(chan struct{}, size),
+		stop:           make(chan struct{}, 1),
+		called:         false,
+		onlyWithLeader: onlyWithLeader,
+	}
+
+	go func() {
+		stop := false
+		for !stop {
+			select {
+			case <-qs.out:
+				var q *quorum
+				if qs.onlyWithLeader {
+					qs.Lock()
+					q = qs.leaderQuorum
+					qs.Unlock()
+				} else {
+					qs.Lock()
+					q = qs.biggestQuorum
+					qs.Unlock()
+				}
+				handler(q)
+			case <-qs.stop:
+				var q *quorum
+				if qs.onlyWithLeader {
+					qs.Lock()
+					q = qs.leaderQuorum
+					qs.Unlock()
+				} else {
+					qs.Lock()
+					q = qs.biggestQuorum
+					qs.Unlock()
+				}
+				handler(q)
+				stop = true
+			}
+		}
+	}()
+
+	go func() {
+		time.Sleep(waitFor)
+		qs.stop <- struct{}{}
+	}()
+
+	return qs
+}
+
+func (q *quorum) getLeaderMsg() interface{} {
+	if q.leaderId != -1 {
+		return q.elements[q.leaderId]
+	}
+	return nil
+}
+
+func (qs *quorumSet) add(e interface{}, fromLeader bool) {
+	qs.Lock()
+	defer qs.Unlock()
+
+	if qs.neededSize < 1 {
+		if !qs.called && qs.wakeupIf() &&
+			(!qs.onlyWithLeader || qs.leaderQuorum != nil) {
+			qs.called = true
+			qs.out <- struct{}{}
+		}
+		return
+	}
+
+	// TODO: deal with duplicates
+	qs.totalSize++
+
+	if qs.called {
+		return
+	}
+
+	for _, q := range qs.quorums {
+		if qs.related(q.elements[0], e) {
+			if q.size >= qs.neededSize {
+				q.elements = append(q.elements, e)
+			} else {
+				q.elements[q.size] = e
+			}
+			if fromLeader && q.leaderId == -1 {
+				q.leaderId = q.size
+				qs.leaderQuorum = q
+			}
+			q.size++
+			if qs.biggestQuorum == nil || q.size > qs.biggestQuorum.size {
+				qs.biggestQuorum = q
+			}
+			if q.size >= qs.neededSize && qs.wakeupIf() &&
+				(!qs.onlyWithLeader || qs.leaderQuorum != nil) {
+				qs.called = true
+				qs.out <- struct{}{}
+			}
+			return
+		}
+	}
+
+	i := len(qs.quorums)
+	q := &quorum{
+		size:     1,
+		leaderId: -1,
+		elements: make([]interface{}, qs.neededSize),
+	}
+	qs.quorums[i] = q
+
+	q.elements[0] = e
+	if fromLeader {
+		q.leaderId = 0
+		qs.leaderQuorum = q
+	}
+	qs.biggestQuorum = q
+
+	if qs.neededSize < 2 && qs.wakeupIf() &&
+		(!qs.onlyWithLeader || qs.leaderQuorum != nil) {
+		qs.called = true
+		qs.out <- struct{}{}
+	}
+}

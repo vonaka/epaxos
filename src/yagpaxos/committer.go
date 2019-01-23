7f59c027c7c7704d87752c7c384fb73bf01bd6dc
vonaka <vonssung@gmail.com>
2019-01-23 16:16:59 +0100

    Add the merge case of the committer reconstruction.

--- src/yagpaxos/committer.go
+++ src/yagpaxos/committer.go
@@ -104,3 +104,83 @@ func (c *committer) undeliveredIter(f func(int32)) {
 		f(c.cmdIds[i])
 	}
 }
+
+type buildingBlock struct {
+	id          int
+	blockLeader int32
+	cmds        []int32
+	nextBlock   *buildingBlock
+}
+
+type committerBuilder struct {
+	headBlock *buildingBlock
+	tailBlock *buildingBlock
+	blocks    map[int32]*buildingBlock
+	maxId     int
+}
+
+func newBuilderFromCommitter(c *committer) *committerBuilder {
+	block := &buildingBlock{
+		id:          0,
+		blockLeader: c.cmdIds[c.last],
+		cmds:        make([]int32, c.last-c.first),
+		nextBlock:   nil,
+	}
+	builder := &committerBuilder{
+		headBlock: block,
+		tailBlock: block,
+		blocks:    make(map[int32]*buildingBlock),
+		maxId:     0,
+	}
+
+	for i := c.first + 1; i <= c.last; i++ {
+		block.cmds[i] = c.cmdIds[i]
+		builder.blocks[c.cmdIds[i]] = block
+	}
+
+	return builder
+}
+
+func (b *committerBuilder) adjust(cmdId int32, dep yagpaxosproto.DepSet) {
+	block, exists := b.blocks[cmdId]
+	if !exists {
+		block = &buildingBlock{
+			id:          b.maxId + 1,
+			blockLeader: cmdId,
+			cmds:        []int32{},
+			nextBlock:   nil,
+		}
+		b.maxId++
+		b.tailBlock.nextBlock = block
+		b.tailBlock = block
+		mergeWith := block
+		dep.Iter(func(cmdId int32) bool {
+			depBlock, exists := b.blocks[cmdId]
+			if exists && depBlock.id < mergeWith.id {
+				mergeWith = depBlock
+			} else if !exists {
+				block.cmds = append(block.cmds, cmdId)
+				b.blocks[cmdId] = block
+			}
+			return false
+		})
+		block.cmds = append(block.cmds, cmdId)
+		b.blocks[cmdId] = block
+		nextBlock := mergeWith.nextBlock
+		for nextBlock != nil {
+			mergeWith.blockLeader = nextBlock.blockLeader
+			for _, id := range nextBlock.cmds {
+				if id != -1 {
+					mergeWith.cmds = append(mergeWith.cmds, id)
+					b.blocks[id] = mergeWith
+				}
+			}
+			nextBlock = nextBlock.nextBlock
+		}
+		mergeWith.nextBlock = nil
+		b.maxId = mergeWith.id
+		b.tailBlock = mergeWith
+	} else {
+		// split
+	}
+}
